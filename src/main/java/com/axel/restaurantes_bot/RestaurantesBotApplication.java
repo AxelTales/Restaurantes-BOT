a51c25afedef6b200e3015388d4c2ea4f6993c8a
Axel <axelcallejas4@gmail.com>
2026-08-30 23:48:46 -0600

    Migra proyecto a Spring Boot (Java), elimina intento anterior en Node

---
+++ src/main/java/com/axel/restaurantes_bot/RestaurantesBotApplication.java
@@ -0,0 +1,13 @@
+package com.axel.restaurantes_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantesBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantesBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axel/restaurantes_bot/RestaurantesBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axel.restaurantes_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantesBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
